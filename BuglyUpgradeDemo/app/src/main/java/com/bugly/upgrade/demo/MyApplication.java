02f587c4b5119895a3e4217042cab72ca1aec285
huangzeyou <huangzeyy@qq.com>
2017-03-26 21:21:01 +0800

    add upgrade activity
    add upgrade activity

--- BuglyUpgradeDemo/app/src/main/java/com/bugly/upgrade/demo/MyApplication.java
+++ BuglyUpgradeDemo/app/src/main/java/com/bugly/upgrade/demo/MyApplication.java
@@ -8,26 +8,29 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.tencent.bugly.Bugly;
 import com.tencent.bugly.beta.Beta;
 import com.tencent.bugly.beta.UpgradeInfo;
 import com.tencent.bugly.beta.ui.UILifecycleListener;
 
+import com.tencent.bugly.beta.upgrade.UpgradeStateListener;
+import com.tencent.bugly.beta.upgrade.UpgradeListener;
+
 
 /**
  * Created by wenjiewu on 2016/5/23.
  */
+
 public class MyApplication extends Application {
 
-    public static final String APP_ID = "900029763"; // TODO 替换成bugly上注册的appid
+    public static final String APP_ID = "91cb66a058"; // TODO 替换成bugly上注册的appid
     public static final String APP_CHANNEL = "DEBUG"; // TODO 自定义渠道
     private static final String TAG = "OnUILifecycleListener";
 
-    @Override
-    public void onCreate() {
-        super.onCreate();
-
+    private void InitBuglyBeta()
+    {
         /**** Beta高级设置*****/
         /**
          * true表示app启动自动初始化升级模块；
@@ -41,7 +44,7 @@ public class MyApplication extends Application {
          * true表示初始化时自动检查升级
          * false表示不会自动检查升级，需要手动调用Beta.checkUpgrade()方法
          */
-        Beta.autoCheckUpgrade = true;
+        Beta.autoCheckUpgrade = false;
 
         /**
          * 设置升级周期为60s（默认检查周期为0s），60s内SDK不重复向后天请求策略
@@ -83,6 +86,53 @@ public class MyApplication extends Application {
         Beta.canShowUpgradeActs.add(MainActivity.class);
 
 
+        /* 设置更新状态回调接口 */
+        Beta.upgradeStateListener = new UpgradeStateListener() {
+            @Override
+            public void onUpgradeSuccess(boolean isManual) {
+                Toast.makeText(getApplicationContext(),"UPGRADE_SUCCESS",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onUpgradeFailed(boolean isManual) {
+                Toast.makeText(getApplicationContext(),"UPGRADE_FAILED",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onUpgrading(boolean isManual) {
+                Toast.makeText(getApplicationContext(),"UPGRADE_CHECKING",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onUpgradeNoVersion(boolean isManual) {
+                Toast.makeText(getApplicationContext(),"UPGRADE_NO_VERSION",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onDownloadCompleted(boolean isManual)
+            {
+                Toast.makeText(getApplicationContext(),"DOWNLOAD_COMPLETED",Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        /*在application中初始化时设置监听，监听策略的收取*/
+        Beta.upgradeListener = new UpgradeListener() {
+            @Override
+            public void onUpgrade(int ret,UpgradeInfo strategy, boolean isManual, boolean isSilence) {
+                if (strategy != null) {
+                    Intent i = new Intent();
+                    i.setClass(getApplicationContext(), UpgradeActivity.class);
+                    i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+                    startActivity(i);
+                    Toast.makeText(MyApplication.this, "有更新 ", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(MyApplication.this, "没有更新", Toast.LENGTH_LONG).show();
+                }
+            }
+        };
+
+
         /**
          *  设置自定义升级对话框UI布局
          *  注意：因为要保持接口统一，需要用户在指定控件按照以下方式设置tag，否则会影响您的正常使用：
@@ -93,7 +143,7 @@ public class MyApplication extends Application {
          *  确定按钮：beta_confirm_button 如：android:tag="beta_confirm_button"
          *  详见layout/upgrade_dialog.xml
          */
-//        Beta.upgradeDialogLayoutId = R.layout.upgrade_dialog;
+        Beta.upgradeDialogLayoutId = R.layout.upgrade_dialog;
 
         /**
          * 设置自定义tip弹窗UI布局
@@ -104,7 +154,7 @@ public class MyApplication extends Application {
          *  确定按钮：beta_confirm_button 如：android:tag="beta_confirm_button"
          *  详见layout/tips_dialog.xml
          */
-//        Beta.tipsDialogLayoutId = R.layout.tips_dialog;
+        Beta.tipsDialogLayoutId = R.layout.tips_dialog;
 
         /**
          *  如果想监听升级对话框的生命周期事件，可以通过设置OnUILifecycleListener接口
@@ -113,7 +163,7 @@ public class MyApplication extends Application {
          *  view - 升级对话框的根布局视图，可通过这个对象查找指定view控件
          *  upgradeInfo - 升级信息
          */
-     /*  Beta.upgradeDialogLifecycleListener = new UILifecycleListener<UpgradeInfo>() {
+        Beta.upgradeDialogLifecycleListener = new UILifecycleListener<UpgradeInfo>() {
             @Override
             public void onCreate(Context context, View view, UpgradeInfo upgradeInfo) {
                 Log.d(TAG, "onCreate");
@@ -162,8 +212,7 @@ public class MyApplication extends Application {
             public void onDestroy(Context context, View view, UpgradeInfo upgradeInfo) {
                 Log.d(TAG, "onDestory");
             }
-        };*/
-
+        };
 
         /**
          * 已经接入Bugly用户改用上面的初始化方法,不影响原有的crash上报功能;
@@ -174,6 +223,7 @@ public class MyApplication extends Application {
          */
         Bugly.init(getApplicationContext(), APP_ID, true);
 
+//        Beta.checkUpgrade();
         /**
          * 如果想自定义策略，按照如下方式设置
          */
@@ -186,6 +236,13 @@ public class MyApplication extends Application {
         //        strategy.setAppChannel(APP_CHANNEL);
 
         //        Bugly.init(getApplicationContext(), APP_ID, true, strategy);
+    }
 
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        InitBuglyBeta();
+
+        Beta.checkUpgrade();
     }
 }
---
+++ BuglyUpgradeDemo/app/src/main/java/com/bugly/upgrade/demo/UpgradeActivity.java
@@ -0,0 +1,161 @@
+package com.bugly.upgrade.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+import com.tencent.bugly.beta.Beta;
+import com.tencent.bugly.beta.UpgradeInfo;
+import com.tencent.bugly.beta.download.DownloadTask;
+import com.tencent.bugly.beta.download.DownloadListener;
+
+public class UpgradeActivity extends AppCompatActivity {
+
+    private TextView tv;
+    private TextView title;
+    private TextView version;
+    private TextView size;
+    private TextView time;
+    private TextView content;
+    private Button cancel;
+    private Button start;
+
+
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_upgrade);
+
+        tv = getView(R.id.tv);
+        title = getView(R.id.title);
+        version = getView(R.id.version);
+        size = getView(R.id.size);
+        time = getView(R.id.time);
+        content = getView(R.id.content);
+        cancel = getView(R.id.cancel);
+        start = getView(R.id.start);
+
+            /*获取下载任务，初始化界面信息*/
+        updateBtn(Beta.getStrategyTask());
+
+
+        DownloadTask task = Beta.getStrategyTask();
+        UpgradeInfo info = Beta.getUpgradeInfo();
+
+        tv.setText(tv.getText().toString() + Beta.getStrategyTask().getSavedLength() + "");
+
+            /*获取策略信息，初始化界面信息*/
+        title.setText(title.getText().toString() + Beta.getUpgradeInfo().title);
+        version.setText(version.getText().toString() + Beta.getUpgradeInfo().versionName);
+        size.setText(size.getText().toString() + Beta.getUpgradeInfo().fileSize + "");
+        time.setText(time.getText().toString() + Beta.getUpgradeInfo().publishTime + "");
+        content.setText(Beta.getUpgradeInfo().newFeature);
+
+            /*为下载按钮设置监听*/
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DownloadTask task = Beta.startDownload();
+                updateBtn(task);
+                if (task.getStatus() == DownloadTask.DOWNLOADING) {
+                    finish();
+                }
+            }
+        });
+
+            /*为取消按钮设置监听*/
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Beta.cancelDownload();
+                finish();
+            }
+        });
+
+            /*注册下载监听，监听下载事件*/
+        Beta.registerDownloadListener(new DownloadListener() {
+            @Override
+            public void onReceive(DownloadTask task) {
+                updateBtn(task);
+                tv.setText(task.getSavedLength() + "");
+            }
+
+            @Override
+            public void onCompleted(DownloadTask task) {
+                updateBtn(task);
+                tv.setText(task.getSavedLength() + "");
+            }
+
+            @Override
+            public void onFailed(DownloadTask task, int code, String extMsg) {
+                updateBtn(task);
+                tv.setText("failed");
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+            /*注销下载监听*/
+        Beta.unregisterDownloadListener();
+    }
+
+
+    public void updateBtn(DownloadTask task) {
+
+            /*根据下载任务状态设置按钮*/
+        switch (task.getStatus()) {
+            case DownloadTask.INIT:
+            case DownloadTask.DELETED:
+            case DownloadTask.FAILED: {
+                start.setText("开始下载");
+            }
+            break;
+            case DownloadTask.COMPLETE: {
+                start.setText("安装");
+            }
+            break;
+            case DownloadTask.DOWNLOADING: {
+                start.setText("暂停");
+            }
+            break;
+            case DownloadTask.PAUSED: {
+                start.setText("继续下载");
+            }
+            break;
+        }
+    }
+
+    public void updateProgress(DownloadTask task)
+    {
+
+    }
+
+    public <T extends View> T getView(int id) {
+        return (T) findViewById(id);
+    }
+}
